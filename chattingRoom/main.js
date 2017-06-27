7af7b552f8fae691696373d990185232f98132ed
baboom3126 <km101km@gmail.com>
2017-06-27 18:14:11 +0800

    Add files via upload

---
+++ chattingRoom/main.js
@@ -0,0 +1,36 @@
+// Initialize Firebase
+
+var config = {
+
+  databaseURL: "https://chatroom-1ca4f.firebaseio.com"
+
+};
+firebase.initializeApp(config);
+
+function send(){
+  var name=document.getElementById("name1").value;
+  var msg=document.getElementById("msg1").value;
+  var room = document.getElementById("room");
+  // room.innerHTML=room.innerHTML+"<p>"+name+"說："+msg+"</p>";
+
+
+  firebase.database().ref('User00001').set({
+  name : name,
+  msg : msg
+  });
+
+document.getElementById("msg1").value="";
+}
+
+
+
+
+  firebase.database().ref('User00001').on("value", function(snapshot) {
+
+    var lat= snapshot.val().name;
+    var lng= snapshot.val().msg;
+
+    room.innerHTML=room.innerHTML+"<p>"+lat+" 說: "+lng+"</p>";
+
+
+  });
